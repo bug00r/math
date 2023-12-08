dfe93ae4fd9aa8b50bd52a8e124cfe35050d69d6
bug00r <bachus@gmx.net>
2023-12-08 11:11:26 +0100

    Starting lz77 Algo

--- algorithm/compression/lz77.c
+++ algorithm/compression/lz77.c
@@ -5,7 +5,7 @@ typedef struct __lz77_buf_pos
 {
     uint8_t *start;
     uint8_t *end;
-} lz77_buf_pos_t;
+} lz77_buf_pos_t, *lz77BufPosPtr;
 
 typedef struct __lz77_sliding_window 
 {
@@ -28,6 +28,59 @@ typedef struct __lz77_context
     /* Definition of lz77 Parameter */ 
     lz77ParamPtr param;    
 } lz77_ctx_t, *lz77CtxPtr;
+
+static bool __lz77_search_buffer_available(lz77CtxPtr _ctx)
+{
+    lz77CtxPtr ctx = _ctx;
+    lz77BufPosPtr searchBufPos = &ctx->slWin.sbBuf;
+
+    return ( searchBufPos->start == NULL || searchBufPos->end == NULL );
+}
+
+static void __lz77_set_search_buffer(lz77CtxPtr _ctx)
+{
+    lz77CtxPtr ctx = _ctx;
+    lz77BufPosPtr searchBufPos = &ctx->slWin.sbBuf;
+    lz77BufPtr srcBuffer = ctx->srcBuf;
+
+    uint8_t *startSrcBuf = &srcBuffer->bytes[0];
+
+    /* no search buffer available */
+    if ( ctx->pos == &srcBuffer->bytes[0] )
+    {
+        searchBufPos->start = NULL;
+        searchBufPos->end = NULL;
+    }
+
+    searchBufPos->start = startSrcBuf - ctx->param->searchBufSize;
+
+    if ( searchBufPos->start < startSrcBuf ) 
+    {
+        searchBufPos->start = startSrcBuf;
+        searchBufPos->end = ctx->pos - 1;
+    }
+        
+}
+
+static void __lz77_set_lookahed_buffer(lz77CtxPtr _ctx)
+{
+    lz77CtxPtr ctx = _ctx;
+    lz77BufPosPtr lookAheadBufPos = &ctx->slWin.laBuf;
+    lz77BufPtr srcBuffer = ctx->srcBuf;
+
+    uint8_t *endSrcBuf = &srcBuffer->bytes[0] + srcBuffer->numBytes;
+
+    //TODO set lookahead Buffer Range with endSrcBuf limit check
+
+}
+
+static void __lz77_set_sliding_window(lz77CtxPtr _ctx)
+{
+    lz77CtxPtr ctx = _ctx;
+
+    __lz77_set_search_buffer(ctx);
+    __lz77_set_lookahed_buffer(ctx);
+}
 
 static void __lz77_init_algo(lz77CtxPtr _ctx, lz77BufPtr srcBuf, lz77BufPtr* dstBuf, lz77ParamPtr param)
 {
--- algorithm/compression/lz77.h
+++ algorithm/compression/lz77.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 typedef struct __lz77_buff 
 {
