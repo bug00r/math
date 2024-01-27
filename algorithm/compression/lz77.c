4e9616fa06641532584982c06244f5f96fa2c34f
bug00r <bug00r@bug0r.com>
2024-01-27 17:14:41 +0100

    adding algorithm structure and process steps

--- algorithm/compression/lz77.c
+++ algorithm/compression/lz77.c
@@ -34,7 +34,7 @@ static bool __lz77_search_buffer_available(lz77CtxPtr _ctx)
     lz77CtxPtr ctx = _ctx;
     lz77BufPosPtr searchBufPos = &ctx->slWin.sbBuf;
 
-    return ( searchBufPos->start == NULL || searchBufPos->end == NULL );
+    return ( searchBufPos->start != searchBufPos->end );
 }
 
 static void __lz77_set_search_buffer(lz77CtxPtr _ctx)
@@ -45,21 +45,30 @@ static void __lz77_set_search_buffer(lz77CtxPtr _ctx)
 
     uint8_t *startSrcBuf = &srcBuffer->bytes[0];
 
-    /* no search buffer available */
-    if ( ctx->pos == &srcBuffer->bytes[0] )
-    {
-        searchBufPos->start = NULL;
-        searchBufPos->end = NULL;
-    } else
-    {
-        searchBufPos->start = startSrcBuf - ctx->param->searchBufSize;
+    uint16_t paramSBufSize = ctx->param->searchBufSize;
+    size_t maxPossSBufSize = ctx->pos - startSrcBuf;
 
-        if ( searchBufPos->start < startSrcBuf ) 
-        {
-            searchBufPos->start = startSrcBuf;
-            searchBufPos->end = ctx->pos - 1;
-        }
+    /* search buffer available */
+    if ( maxPossSBufSize )
+    {
+        uint16_t curSBufSize = ( maxPossSBufSize < paramSBufSize ? maxPossSBufSize : paramSBufSize );
+        
+        searchBufPos->start = ctx->pos - 1;
+        searchBufPos->end = ctx->pos - curSBufSize;
+    } 
+    else
+    {
+        searchBufPos->start = ctx->pos;
+        searchBufPos->end = ctx->pos;
     }        
+}
+
+static bool __lz77_lookahed_buffer_available(lz77CtxPtr _ctx)
+{
+    lz77CtxPtr ctx = _ctx;
+    lz77BufPosPtr lookAheadBufPos = &ctx->slWin.laBuf;
+
+    return ( lookAheadBufPos->start != lookAheadBufPos->end );
 }
 
 static void __lz77_set_lookahed_buffer(lz77CtxPtr _ctx)
@@ -68,9 +77,23 @@ static void __lz77_set_lookahed_buffer(lz77CtxPtr _ctx)
     lz77BufPosPtr lookAheadBufPos = &ctx->slWin.laBuf;
     lz77BufPtr srcBuffer = ctx->srcBuf;
 
-    uint8_t *endSrcBuf = &srcBuffer->bytes[0] + srcBuffer->numBytes;
+    uint8_t *endSrcBuf = &srcBuffer->bytes[0] + srcBuffer->numBytes - 1;
+    
+    uint16_t paramLaBufSize = ctx->param->lookaheadBufSize;
+    size_t maxPossLaBufSize = endSrcBuf - ctx->pos;
 
-    //TODO set lookahead Buffer Range with endSrcBuf limit check
+    if ( maxPossLaBufSize )
+    {
+        uint16_t curLaBufSize = ( maxPossLaBufSize < paramLaBufSize ? maxPossLaBufSize : paramLaBufSize );
+
+        lookAheadBufPos->start = ctx->pos;
+        lookAheadBufPos->start = ctx->pos + curLaBufSize;
+    }
+    else 
+    {
+        lookAheadBufPos->start = ctx->pos;
+        lookAheadBufPos->end = ctx->pos;
+    }
 
 }
 
@@ -88,7 +111,54 @@ static void __lz77_init_algo(lz77CtxPtr _ctx, lz77BufPtr srcBuf, lz77BufPtr* dst
     ctx->pos = &srcBuf->bytes[0]; 
     ctx->srcBuf = srcBuf;
     ctx->dstBuf = dstBuf;
+    
+    //Destination Buffer will be cleared at Beginning
+    *ctx->dstBuf = NULL;
+
     ctx->param = param;
+}
+
+/* currently not using bitfields because lib should be usable with c99 too*/
+typedef struct __lz77_triplet 
+{
+    uint8_t bytes[3];
+} lz77_priplet_t,*lz77TripletPtr;
+
+static void __lz77_search_next_triplet(lz77CtxPtr _ctx, lz77TripletPtr _curTriplet)
+{
+    lz77CtxPtr ctx = _ctx;
+    lz77TripletPtr curTriplet = _curTriplet;
+
+    //todo searching with every special situation
+
+}
+
+static void __lz77_dump_triplet_to_dst_buffer(lz77CtxPtr _ctx, lz77TripletPtr _curTriplet)
+{
+    lz77CtxPtr ctx = _ctx;
+    lz77TripletPtr curTriplet = _curTriplet;
+
+    /*  todo dumping triplet into dst buffer maybe allocating 
+        chunks with 64,128, 256, 512 etc blocks(reducing sys calls for memory)
+    */
+}
+
+static void __lz77_encode(lz77CtxPtr _ctx)
+{
+    lz77CtxPtr ctx = _ctx;
+    lz77_priplet_t curTriplet;
+    lz77TripletPtr curTripletPtr = &curTriplet;
+
+    while ( __lz77_lookahed_buffer_available(ctx) )
+    {
+        __lz77_set_search_buffer(ctx);
+
+        /* mybe __lz77_search_buffer_available is not needed */
+        __lz77_search_next_triplet(ctx, curTripletPtr);
+
+        __lz77_dump_triplet_to_dst_buffer(ctx, curTripletPtr);
+    }
+
 }
 
 /***** PUBLIC INTERFACE *****/
